e014b20606299cdd88be42f4ff0a7cf82e7672bf
jysaa5 <jysaa5@naver.com>
2023-03-10 01:25:07 +0900

    next.js 프로젝트 생성

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,16 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "/Users/lilac/Workspace/GitHub/Side_Project/csnojam-frontend/src/app/page": [
+      "/Users/lilac/Workspace/GitHub/Side_Project/csnojam-frontend/src/app/page.module.css",
+      "/Users/lilac/Workspace/GitHub/Side_Project/csnojam-frontend/.yarn/__virtual__/next-virtual-4c0e0bc800/0/cache/next-npm-13.2.3-5bd86c8056-5147078ceb.zip/node_modules/next/font/google/target.css?{\"path\":\"src/app/page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/Users/lilac/Workspace/GitHub/Side_Project/csnojam-frontend/src/app/globals.css"
+    ]
+  },
+  "/Users/lilac/Workspace/GitHub/Side_Project/csnojam-frontend/src/app/page.tsx": [
+    "/Users/lilac/Workspace/GitHub/Side_Project/csnojam-frontend/src/app/page.module.css",
+    "/Users/lilac/Workspace/GitHub/Side_Project/csnojam-frontend/.yarn/__virtual__/next-virtual-4c0e0bc800/0/cache/next-npm-13.2.3-5bd86c8056-5147078ceb.zip/node_modules/next/font/google/target.css?{\"path\":\"src/app/page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+  ],
+  "/Users/lilac/Workspace/GitHub/Side_Project/csnojam-frontend/src/app/layout.tsx": [
+    "/Users/lilac/Workspace/GitHub/Side_Project/csnojam-frontend/src/app/globals.css"
+  ]
+}
---
+++ .next/static/chunks/app/layout.js
@@ -0,0 +1,28 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([["app/layout"],{
+
+/***/ "(app-client)/./.yarn/__virtual__/next-virtual-4c0e0bc800/0/cache/next-npm-13.2.3-5bd86c8056-5147078ceb.zip/node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2Flilac%2FWorkspace%2FGitHub%2FSide_Project%2Fcsnojam-frontend%2Fsrc%2Fapp%2Fglobals.css&server=false!":
+/*!*************************************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./.yarn/__virtual__/next-virtual-4c0e0bc800/0/cache/next-npm-13.2.3-5bd86c8056-5147078ceb.zip/node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2Flilac%2FWorkspace%2FGitHub%2FSide_Project%2Fcsnojam-frontend%2Fsrc%2Fapp%2Fglobals.css&server=false! ***!
+  \*************************************************************************************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("(() => __webpack_require__.e(/*! import() */ \"_app-client_src_app_globals_css\").then(__webpack_require__.bind(__webpack_require__, /*! ./src/app/globals.css */ \"(app-client)/./src/app/globals.css\")))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1jbGllbnQpLy4vLnlhcm4vX192aXJ0dWFsX18vbmV4dC12aXJ0dWFsLTRjMGUwYmM4MDAvMC9jYWNoZS9uZXh0LW5wbS0xMy4yLjMtNWJkODZjODA1Ni01MTQ3MDc4Y2ViLnppcC9ub2RlX21vZHVsZXMvbmV4dC9kaXN0L2J1aWxkL3dlYnBhY2svbG9hZGVycy9uZXh0LWZsaWdodC1jbGllbnQtZW50cnktbG9hZGVyLmpzP21vZHVsZXM9JTJGVXNlcnMlMkZsaWxhYyUyRldvcmtzcGFjZSUyRkdpdEh1YiUyRlNpZGVfUHJvamVjdCUyRmNzbm9qYW0tZnJvbnRlbmQlMkZzcmMlMkZhcHAlMkZnbG9iYWxzLmNzcyZzZXJ2ZXI9ZmFsc2UhLmpzIiwibWFwcGluZ3MiOiJBQUFBLE9BQU8sK0xBQW1IIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vX05fRS8/MWQ3YyJdLCJzb3VyY2VzQ29udGVudCI6WyIoKCkgPT4gaW1wb3J0KC8qIHdlYnBhY2tNb2RlOiBcImxhenlcIiAqLyBcIi9Vc2Vycy9saWxhYy9Xb3Jrc3BhY2UvR2l0SHViL1NpZGVfUHJvamVjdC9jc25vamFtLWZyb250ZW5kL3NyYy9hcHAvZ2xvYmFscy5jc3NcIikpIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///(app-client)/./.yarn/__virtual__/next-virtual-4c0e0bc800/0/cache/next-npm-13.2.3-5bd86c8056-5147078ceb.zip/node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2Flilac%2FWorkspace%2FGitHub%2FSide_Project%2Fcsnojam-frontend%2Fsrc%2Fapp%2Fglobals.css&server=false!\n"));
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+/******/ __webpack_require__.O(0, ["main-app"], function() { return __webpack_exec__("(app-client)/./.yarn/__virtual__/next-virtual-4c0e0bc800/0/cache/next-npm-13.2.3-5bd86c8056-5147078ceb.zip/node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2Flilac%2FWorkspace%2FGitHub%2FSide_Project%2Fcsnojam-frontend%2Fsrc%2Fapp%2Fglobals.css&server=false!"); });
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ _N_E = __webpack_exports__;
+/******/ }
+]);
